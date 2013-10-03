aff34bbc343ff94fa21975a097b1c43587030f8a
Dyego Reis de Azevedo <dyegoreisa@gmail.com>
2013-10-03 04:35:19 +0000

    Finalização da implementação do modelo Turma e Professor. Implmentação do DAO para Turma e Professor

--- src/br/com/senac/dao/Conexao.java
+++ src/br/com/senac/dao/Conexao.java
@@ -13,7 +13,7 @@ public class Conexao {
 			Class.forName("org.postgresql.Driver");
 			conexao = DriverManager
 					.getConnection(
-							"jdbc:postgresql://localhost:5432/AcademicNet",
+							"jdbc:postgresql://localhost:5432/academicnetsenac",
 							"postgres", "banco123");
 		} catch (Exception e) {
 			e.printStackTrace();
