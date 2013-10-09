30f304b70b509c58c13bb75ba08b7deb478c510c
Dyego Reis de Azevedo <dyegoreisa@gmail.com>
2013-10-09 06:56:49 +0000

    Implementação de telas

---
+++ src/br/com/senac/view/GUI/TelaListaProfessor.java
@@ -0,0 +1,92 @@
+package br.com.senac.view.GUI;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.KeyEvent;
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSeparator;
+import javax.swing.JTextField;
+
+public class TelaListaProfessor extends JFrame {
+
+	public TelaListaProfessor () {
+		initUI();
+	}
+	
+	private final void initUI() {
+		
+		JPanel basic = new JPanel();
+		basic.setLayout(new BoxLayout(basic, BoxLayout.Y_AXIS));
+		add(basic);
+		
+		JPanel topPanel = new JPanel(new BorderLayout(0, 0));
+		topPanel.setMaximumSize(new Dimension(450, 0));
+		JLabel title = new JLabel("Lista de professores");
+		title.setBorder(BorderFactory.createEmptyBorder(0, 25, 0, 0));
+		topPanel.add(title);
+		
+		ImageIcon icon = new ImageIcon(getClass().getResource("/images/professor48x48.png"));
+        JLabel label = new JLabel(icon);
+        label.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+        topPanel.add(label, BorderLayout.WEST);
+
+        JSeparator separator = new JSeparator();
+        separator.setForeground(Color.gray);
+
+        topPanel.add(separator, BorderLayout.SOUTH);
+
+        basic.add(topPanel);
+
+        JPanel fieldPanel = new JPanel();
+        fieldPanel.setLayout(new GridLayout(10, 2));
+               
+        Font fontLabel = new Font("Verdana", Font.PLAIN, 14);
+        Color colorLabel = new Color(50, 50, 25);
+        
+        String[] campos = {"ID:", "Nome:", "Sobrenome:", "Sexo:", "Telefone:", "Data de Nacimento:",
+        		"E-mail:", "Especialidade:", "Salário:", "Víndulo"};
+        
+        for (int i = 0; i < campos.length; i++) {
+            JLabel lbl = new JLabel(campos[i]);
+            lbl.setFont(fontLabel);
+            lbl.setForeground(colorLabel);
+            fieldPanel.add(lbl, BorderLayout.WEST);
+            fieldPanel.add(new JTextField(), BorderLayout.EAST);        	
+        }
+
+        fieldPanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+        basic.add(fieldPanel);      
+
+        JPanel bottom = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+
+        JButton btnSalvar = new JButton("Salvar");
+        btnSalvar.setMnemonic(KeyEvent.VK_S);
+        JButton btnFechar = new JButton("Fechar");
+        btnFechar.setMnemonic(KeyEvent.VK_F);
+
+        bottom.add(btnSalvar);
+        bottom.add(btnFechar);
+        basic.add(bottom);
+
+        bottom.setMaximumSize(new Dimension(450, 0));
+
+        setTitle("Professor");
+        setSize(new Dimension(450, 350));
+        setResizable(false);
+        setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+        setLocationRelativeTo(null);
+	}
+	
+}
