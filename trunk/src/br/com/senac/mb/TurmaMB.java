7fb8a97aeabdc869660ed7e6bed0ec21e375fc5d
dyegoreisa@gmail.com <dyegoreisa@gmail.com@1ed618b4-70bf-ef2f-6c7c-d813e6c9869e>
2014-05-02 10:14:34 +0000

    Implementação da turma

---
+++ trunk/src/br/com/senac/mb/TurmaMB.java
@@ -0,0 +1,117 @@
+package br.com.senac.mb;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+import br.com.senac.dao.CursoDAO;
+import br.com.senac.dao.TurmaDAO;
+import br.com.senac.model.Curso;
+import br.com.senac.model.Turma;
+
+@ManagedBean(name="turmaMB")
+@RequestScoped
+public class TurmaMB {
+
+	private Turma turma;
+	private TurmaDAO turmaDAO;
+	private CursoDAO cursoDAO;
+	private List<Turma> turmas;
+	private List<Curso> cursos;
+
+	private int acao;
+	
+	/**
+	 * Campo usado para busca
+	 */
+	private String conteudo;
+
+	public TurmaMB() {
+		turma = new Turma();
+		turmaDAO = new TurmaDAO();
+		cursoDAO = new CursoDAO();
+		acao = 1;
+	}
+
+	public Turma getTurma() {
+		return turma;
+	}
+
+	public void setTurma(Turma turma) {
+		this.turma = turma;
+	}
+	
+	public List<Turma> getTurmas() {
+		if (turmas == null) {
+			turmas = turmaDAO.listar();
+		}
+		return turmas;
+	}
+
+	public void setTurmas(List<Turma> turmas) {
+		this.turmas = turmas;
+	}
+
+	public String getConteudo() {
+		return conteudo;
+	}
+
+	public void setConteudo(String conteudo) {
+		this.conteudo = conteudo;
+	}
+		
+	public int getAcao() {
+		return acao;
+	}
+	
+	public void setAcao(int acao) {
+		this.acao = acao;
+	}
+
+	public List<Curso> getCursos() {
+		if (cursos == null) {
+			cursos = cursoDAO.listar();
+		}
+		return cursos;
+	}
+	/*
+	 * Comandos 
+	 */
+
+	public String goUpdate(Turma turmaSelecionado) {
+		turma = turmaDAO.getById(turmaSelecionado.getId());
+		acao = 2;
+		return "cadastrarTurma";
+	}
+
+	public String salvar() {
+		switch(acao)
+		{
+			case 1: // Inserir
+				turmaDAO.inserir(turma);
+				break;
+				
+			case 2: // Alterar
+				turmaDAO.atualizar(turma);
+				break;
+		}
+		
+		return "listarTurma";
+	}
+	
+	public void excluir(Turma turmaSelecionado) {
+		turmaDAO.apagar(turmaSelecionado);
+		turmas = turmaDAO.listar();
+	}
+	
+	public List<Turma> listar() {
+		return getTurmas();
+	}
+	
+	public void buscar() {
+		turmas = turmaDAO.buscar(conteudo.toString());
+	}
+}
